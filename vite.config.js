60a7551f8a0586e8e926556f653cf8d9e757906b
nichoth <nichoth@nichoth.com>
2026-05-29 14:26:39 -0700

    use lightningcss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import postcssNesting from 'postcss-nesting'
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +14,10 @@ export default defineConfig({
     },
     publicDir: '_public',
     css: {
-        postcss: {
-            plugins: [
-                postcssNesting
-            ],
-        },
+        transformer: 'lightningcss',
+        lightningcss: {
+            targets: browserslistToTargets(browserslist())
+        }
     },
     server: {
         port: 8888,
